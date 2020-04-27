8e8aff0bf567976835bf7015fbeeacfc8f31d3f4
cancer1725 <cancer1725@gmail.com>
2020-04-27 14:57:07 +0900

    1 file modified

--- src/app/services/global.service.ts
+++ src/app/services/global.service.ts
@@ -1,27 +1,28 @@
 /* Copyright (c) 2018-2019 VMware, Inc. All rights reserved. */
 
-import {Injectable} from '@angular/core';
-import {Headers, Http} from '@angular/http';
+import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
 
 @Injectable()
 export class GlobalService {
-   protected static WEB_CONTEXT_PATH = '/ui/html-sample';
+  protected static WEB_CONTEXT_PATH = '/ui/html-sample';
 
-   protected headers = new Headers(
-         {
-            'Content-Type': 'application/json',
-            'Cache-Control': 'no-cache, no-store, max-age=0, must-revalidate',
-            'Pragma': 'no-cache',
-            'Expires': '0'
-         });
-   public readonly htmlClientSdk: any;
+  protected headers = new Headers({
+    'Content-Type': 'application/json',
+    'Cache-Control': 'no-cache, no-store, max-age=0, must-revalidate',
+    Pragma: 'no-cache',
+    Expires: '0'
+  });
+  public readonly htmlClientSdk: any;
 
-   constructor(protected http: Http) {
-      //this.htmlClientSdk = (<any>window.frameElement).htmlClientSdk;
-   }
+  constructor(protected http: Http) {
+    if (this.htmlClientSdk != null) {
+      this.htmlClientSdk = (<any>window.frameElement).htmlClientSdk;
+    }
+  }
 
-   protected handleError(error: any): Promise<any> {
-      const errMsg = error.message || 'Server error: check console for details';
-      return Promise.reject(errMsg);
-   }
+  protected handleError(error: any): Promise<any> {
+    const errMsg = error.message || 'Server error: check console for details';
+    return Promise.reject(errMsg);
+  }
 }
